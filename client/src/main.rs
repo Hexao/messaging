a4d1f8db1a9ae7a1e10e76f45ab464056bcfdac6
Alexandre ANDRE <aa.alexandre.andre@gmail.com>
2021-11-17 17:00:43 +0100

    switch to rust2021

--- client/src/main.rs
+++ client/src/main.rs
@@ -9,13 +9,18 @@ use image::{GenericImageView, io::Reader as ImReader};
 
 pub fn main() {
     let path = "./resources/aircraft.png";
-    let icon = (|| {
-        let img = ImReader::open(path).ok()?.decode().ok()?;
-        let (width, height) = img.dimensions();
-        let rgba = img.into_rgba8().into_raw();
+    let icon = match ImReader::open(path) {
+        Ok(buffer) => match buffer.decode() {
+            Ok(img) => {
+                let (width, height) = img.dimensions();
+                let rgba = img.into_rgba8().into_raw();
 
-        Icon::from_rgba(rgba, width, height).ok()
-    })();
+                Icon::from_rgba(rgba, width, height).ok()
+            }
+            _ => None,
+        }
+        _ => None,
+    };
 
     Client::run(Settings {
         window: window::Settings {
